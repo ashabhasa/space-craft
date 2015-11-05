ed3d34e3b0d7049bccfb96430d4e55a1326225b1
ashabhasa <ashabhasa@gmail.com>
2015-11-05 20:06:36 +0100

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to space-craft
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/space_craft/core_test.clj
@@ -0,0 +1,7 @@
+(ns space-craft.core-test
+  (:require [clojure.test :refer :all]
+            [space-craft.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
