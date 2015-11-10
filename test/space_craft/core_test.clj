ce9f8e3745bd8255a453b8de5a53f60882c2712d
ashabhasa <ashabhasa@gmail.com>
2015-11-10 20:03:04 +0100

    Space craft project

--- test/space_craft/core_test.clj
+++ test/space_craft/core_test.clj
@@ -2,6 +2,25 @@
   (:require [clojure.test :refer :all]
             [space-craft.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest spherical-coordinates-test
+  (let [pos {:x 1.0 :y 2.0 :z 3.0}]
+    (testing "roundtrip"
+      (is (= pos (-> pos cartesian->spherical spherical->cartesian)))))
+  (testing "spherical->cartesian"
+    (is (= (spherical->cartesian {:r     2
+                                  :phi   0
+                                  :theta 0})
+           {:x 0.0 :y 0.0 :z 2.0}))))
+
+
+(deftest makes-orbit
+  (let [trajectory (->> (atlas-v)
+                        prepare
+                        (trajectory 100))]
+    (when (crashed? trajectory)
+      (println "Crashed at" (crash-time trajectory) "seconds")
+
+      (println "Maximum altitude" (apoapsis trajectory)
+               "meters at" (apoapsis-time trajectory) "seconds")
+      )
+    (is (not (crashed? trajectory)))))
